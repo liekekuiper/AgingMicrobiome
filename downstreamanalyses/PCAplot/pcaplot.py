f28831c284b4f3242f0c56bfc7330b1362e3b480
liekekuiper <114936730+liekekuiper@users.noreply.github.com>
2025-01-17 14:13:12 +0100

    Create pcaplot.py

---
+++ downstreamanalyses/PCAplot/pcaplot.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+scores = pd.read_csv('combined_pca_scores.csv', index_col = 0)
+explained_variance = pd.read_csv('explained_variance.csv', index_col = 0)
+
+# Plot PCA
+    sns.set(style="whitegrid")
+    plt.figure(figsize=(10, 8))
+    sns.scatterplot(data=scores, x="PC1", y="PC2", hue="Cohort", palette="Set2", s=100, alpha=0.5)
+    plt.title("PCA of CLR-Transformed Data for Top 4 Genera (Grouped by Cohort)", fontsize=14)
+    plt.xlabel(f"PC1 ({explained_variance['0'][0]*100:.1f}%)")
+    plt.ylabel(f"PC2 ({explained_variance['0'][1]*100:.1f}%)")
+    plt.legend(title="Cohort", loc="best", fontsize=10)
+    plt.tight_layout()
+    plt.savefig(f"{output_dir}/combined_pca_plot.png")
+    plt.show()
