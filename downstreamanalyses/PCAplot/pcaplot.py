6ae50d331c5ddda4c1217d79f9d9560adad6fae9
liekekuiper <114936730+liekekuiper@users.noreply.github.com>
2025-01-17 14:36:46 +0100

    Update pcaplot.py

--- downstreamanalyses/PCAplot/pcaplot.py
+++ downstreamanalyses/PCAplot/pcaplot.py
@@ -1,6 +1,10 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-scores = pd.read_csv('combined_pca_scores.csv', index_col = 0)
+scores_server1 = pd.read_csv('combined_pca_scores.csv', index_col = 0)
+scores_dcs = pd.read_csv('pca_scores_DCS_common_genera.csv', index_col = 0)
+scores = scores_server1.merge(scores_dcs, how = 'outer')
 explained_variance = pd.read_csv('explained_variance.csv', index_col = 0)
 
 # Plot PCA
