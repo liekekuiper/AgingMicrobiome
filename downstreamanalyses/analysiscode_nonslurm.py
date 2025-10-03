c0b7930956a60f7be23918a4b15f6ecdf9b8a13f
liekekuiper <114936730+liekekuiper@users.noreply.github.com>
2025-10-03 08:49:59 +0200

    Small fix analysiscode_nonslurm.py

--- downstreamanalyses/analysiscode_nonslurm.py
+++ downstreamanalyses/analysiscode_nonslurm.py
@@ -4,44 +4,40 @@ import numpy as np
 import statsmodels.api as sm
 from lifelines import CoxPHFitter
 from lifelines.utils import k_fold_cross_validation
-from microbiome_utils import *
+from microbiome_utils_fixed_alex3 import *
 from patsy import dmatrices
 
 # Fill in the right filepaths
-label = '16s/metagenomics' #keep the one for which the analyses are run 
-taxonomy = '2022.10.taxonomy.asv.tsv.qza' #path to taxonomy file
-tree = '2022.10.phylogeny.asv.nwk.qza' #path to tree
-feature_table = 'feature.table.gg2-2022.10.qza' #path to feature table
-metadata = 'metadata.txt' #path to metadata
-threads = int(6) #change threads if needed
-modsfile = 'mods_agingmicrobiome.txt' #path to file with models
-outsfile = 'outs_agingmicrobiome.txt' #path with files with outcomes
-cohortname = 'cohort.txt' #path to file with cohort name
-subsfile = 'subsets_agingmicrobiome.txt' #path to file with relevant subsets
-factors_str = 'NA' #Are there any study-specific variables that need to be added that are a factor, add those here, in case of >1 'factor1,factor2'
+label = '16s/metagenomics'
+taxonomy = '2022.10.taxonomy.asv.tsv.qza'
+tree = '2022.10.phylogeny.asv.nwk.qza'
+feature_table = 'feature.table.gg2-2022.10.qza'
+metadata = 'metadata.txt'
+threads = int(6)
+modsfile = 'mods_agingmicrobiome.txt'
+outsfile = 'outs_agingmicrobiome.txt'
+cohortname = 'cohort.txt'
+subsfile = 'subsets_agingmicrobiome.txt'
+factors_str = 'NA'
 
 #----------------------------------------------------#
 ##   Do not change anything underneath these lines  ##
 #----------------------------------------------------#
 
-# Strip any leading or trailing quotes
 if factors_str.startswith("'") and factors_str.endswith("'"):
     factors_str = factors_str[1:-1]
 
-factors = factors_str.split(',')  # Split the string into a list
+factors = factors_str.split(',')
 
 print(threads)
 print(factors)
-
 print('imported environments')
 
-# Prepare dataframe
 results_df = pd.DataFrame(columns=[
     'Datasplit', 'Outcome', 'Variable', 'Model', 'N', 'Ncases', 'Coefficient',
     'Std.Error', 'HR', 'LL','UL','t.value', 'P'
 ])
 
-# Load data
 with open(modsfile, 'r') as file:
     models = file.read().splitlines()
 with open(outsfile, 'r') as file:
@@ -57,7 +53,6 @@ variables_store = pd.read_csv(metadata, sep='\t', dtype=str).columns.str.lower()
 
 print('stored variables, will start for loop')
 
-#Run analyses
 for subs in subsets:
     print(f"Performing analyses of {subs}")
     for out in outcomes:
@@ -67,7 +62,7 @@ for subs in subsets:
                 model = f"{model}+age"
             if "men" in subs:
                 model = model.replace("sex+", "").replace("sex", "")
-            model_terms = model.split('+')  # Split the model string into a list of terms
+            model_terms = model.split('+')
             datafile = process(
                 taxonomy=taxonomy,
                 tree=tree,
@@ -78,11 +73,14 @@ for subs in subsets:
                 model=model,
                 sub=subs,
                 out=out,
-                factors=factors
+                factors=factors,
+                label=label
             )
-            # Replace all hyphens with underscores in the column names as otherwise encounter patsy error
             datafile.columns = datafile.columns.str.replace('-', '_')
-            print('created diversity matrices, now continue with analyses')
+            datafile.columns = datafile.columns.str.replace(' ', '_')
+            datafile.columns = datafile.columns.str.replace('[^0-9a-zA-Z_]', '', regex=True)
+
+            print('created dataset, now continue with analyses')
             variables = [
                 col for col in datafile.columns 
                 if col not in variables_store and not any(col.startswith(term + '_') for term in model_terms)
@@ -92,7 +90,7 @@ for subs in subsets:
                     formula = f"{out} ~ {model} + {var}"
                     datafile[out] = pd.to_numeric(datafile[out])
                     y, X = dmatrices(formula, data=datafile, return_type='dataframe')
-                    lmmodel = sm.OLS(y, X).fit()               
+                    lmmodel = sm.OLS(y, X).fit()
                     coefficients = lmmodel.summary2().tables[1]
                     variable = var
                     result_row = pd.DataFrame([{
@@ -110,37 +108,42 @@ for subs in subsets:
                         't.value': coefficients.loc[variable, 't'],
                         'P': coefficients.loc[variable, 'P>|t|']
                     }])
+                    results_df = pd.concat([results_df, result_row], ignore_index=True)
                 else:
                     datafile['followup'] = datafile['studytime'] + datafile['age']
                     covariates = model_terms + [var]
-                    covariates = [cov.strip() for cov in covariates]  # Ensure no leading/trailing spaces
-                    # Get final list of covariates after encoding
+                    covariates = [cov.strip() for cov in covariates]
                     covariates_use = [col for col in datafile.columns if any(cov in col for cov in covariates)]
                     cph = CoxPHFitter()
-                    cph.fit(datafile[covariates_use + ['followup', 'mortality']], duration_col='followup', event_col='mortality')
-                    coefficients = cph.summary
-                    variable = var
-                    result_row = pd.DataFrame([{
-                        'Datasplit': subs,
-                        'Outcome': out,
-                        'Variable': var,
-                        'Model': model,
-                        'N': cph._n_examples,
-                        'Ncases': cph.event_observed.sum(),
-                        'Coefficient': coefficients.loc[variable, 'coef'],
-                        'Std.Error': coefficients.loc[variable, 'se(coef)'],
-                        'HR': coefficients.loc[variable, 'exp(coef)'],
-                        'LL': coefficients.loc[variable, 'exp(coef) lower 95%'],
-                        'UL': coefficients.loc[variable, 'exp(coef) upper 95%'],
-                        't.value': np.nan,
-                        'P': coefficients.loc[variable, 'p']
-                    }])
-                results_df = pd.concat([results_df, result_row], ignore_index=True)
-            results_df.to_csv(f"./intermediatefiles/Results_{subs}_{cohort}_{pd.Timestamp.today().date()}.csv", index=False)
-            model = original_model  # Reset the model to its original state for the next iteration
+                    try:
+                        cph.fit(datafile[covariates_use + ['followup', 'mortality']], duration_col='followup', event_col='mortality')
+                        coefficients = cph.summary
+                        variable = var
+                        result_row = pd.DataFrame([{
+                            'Datasplit': subs,
+                            'Outcome': out,
+                            'Variable': var,
+                            'Model': model,
+                            'N': cph._n_examples,
+                            'Ncases': cph.event_observed.sum(),
+                            'Coefficient': coefficients.loc[variable, 'coef'],
+                            'Std.Error': coefficients.loc[variable, 'se(coef)'],
+                            'HR': coefficients.loc[variable, 'exp(coef)'],
+                            'LL': coefficients.loc[variable, 'exp(coef) lower 95%'],
+                            'UL': coefficients.loc[variable, 'exp(coef) upper 95%'],
+                            't.value': np.nan,
+                            'P': coefficients.loc[variable, 'p']
+                        }])
+                        results_df = pd.concat([results_df, result_row], ignore_index=True)
+                    except Exception as e:
+                        print(f"Failed CoxPH fit for variable '{var}' in subset '{subs}' with outcome '{out}'. Error: {e}")
+                        print(f"Covariates used: {covariates_use}")
+                        continue
+            results_df.to_csv(f"./intermediatefiles/Results_{label}_{subs}_{cohort}_{pd.Timestamp.today().date()}.csv", index=False)
+            model = original_model
     print(f"Finished analyses of {subs}")
 
 results_df.to_csv(f"Results_{cohort}_{label}_{pd.Timestamp.today().date()}.csv", index=False)
 
 if __name__ == '__main__':
-    process()  # Ensuring the click context for process() to work if this script is run standalone
+    process()
