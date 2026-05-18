880f2d6fcc80acccf1221519ed3ad0b63ec8fbd6
liekekuiper <114936730+liekekuiper@users.noreply.github.com>
2026-05-18 10:34:02 +0200

    Add submit folder to woltka_batch.sh

--- preprocessing/shotgun/PreProcessingUpdate/woltka_batch.sh
+++ preprocessing/shotgun/PreProcessingUpdate/woltka_batch.sh
@@ -5,25 +5,22 @@
 #SBATCH -o slurm-%x-%j.out
 #SBATCH -e slurm-%x-%j.err
 
-
-### If you want functions run the following two lines (without the #) in your wol2 folder
-# wget -r -np -nH --cut-dirs=2 http://ftp.microbio.me/pub/wol2/function/
-# wget -r -np -nH --cut-dirs=2 http://ftp.microbio.me/pub/wol2/genomes/
-# wget -r -np -nH --cut-dirs=2 http://ftp.microbio.me/pub/wol2/proteins/
-
-#if not turn on --no-fun
+# Runs wol2sop.sh on the aligned .sam files for a given batch.
+# To include functional profiling, download the following into your wol2 directory:
+#   wget -r -np -nH --cut-dirs=2 http://ftp.microbio.me/pub/wol2/function/
+#   wget -r -np -nH --cut-dirs=2 http://ftp.microbio.me/pub/wol2/genomes/
+#   wget -r -np -nH --cut-dirs=2 http://ftp.microbio.me/pub/wol2/proteins/
+# If not interested in functions remove # in front of --no-fun below.
 
 set -euo pipefail
 
 BATCH_ID=$1
 BATCH_DIR=$(printf "Batch%02d" "$BATCH_ID")
 
-cd "$SLURM_SUBMIT_DIR/$BATCH_DIR"
-
-./wol2sop.sh \
-  -d ../wol2 \ #Change into your wol2 directory
-  -i align \
-  -o Output \
+"$SLURM_SUBMIT_DIR/wol2sop.sh" \
+  -d "$SLURM_SUBMIT_DIR/wol2" \
+  -i "$SLURM_SUBMIT_DIR/$BATCH_DIR/align" \
+  -o "$SLURM_SUBMIT_DIR/$BATCH_DIR/Output" \
   -f biom \
   --no-tax
-#--no-fun
+# --no-fun
