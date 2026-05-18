a2b6dab4796de0e92ff82650cb8632acaedb3ac9
liekekuiper <114936730+liekekuiper@users.noreply.github.com>
2026-05-18 10:45:37 +0200

    Create config.sh

---
+++ preprocessing/shotgun/PreProcessingUpdate/config.sh
@@ -0,0 +1,4 @@
+# Shared configuration for all batch scripts
+MANIFEST=manifest    # Path to the sample manifest file
+BATCH_SIZE=10        # Number of samples per batch
+DB=./db              # Path to bowtie2 database index
